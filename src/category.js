8b4530b6996a499bafaa288be3725beeb48b69c1
jacmac2812 <simonsen.jac@icloud.com>
2020-12-03 10:38:47 +0100

    added category

--- src/category.js
+++ src/category.js
@@ -1,49 +1,40 @@
 import facade from "./apiFacade";
 import React, { useState, useEffect } from "react";
-import Product from "./product";
 
 const Category = () => {
     const [categoryData, setCategoryData] = useState("");
 
-
-
-    useEffect(() => {
-        
-        facade.fetchProductsData().then((data) => setCategoryData(data.products));
-    }, []);
-
     function categoryId(id) {
-        alert(`${id}`);
-      }
-
+        facade.fetchCategoriesData(id).then((data) => setCategoryData(data.products));
+    }
+    //<a href={category.mobileUrl}>Køb her</a>
     return (
         <div>
-<button onClick={() => categoryId('abcat0101000')}>Tv's</button>
-<button onClick={() => categoryId('abcat0102000')}>Blu-ray & DVD Players</button>
-<button onClick={() => categoryId('abcat0106021')}>Gamer Stole</button>
-<button onClick={() => categoryId('abcat0200000')}>Lyd</button>
-<button onClick={() => categoryId('abcat0201000')}>iPod & MP3 Players</button>
-<button onClick={() => categoryId('abcat0204000')}>Headphones</button>
-<button onClick={() => categoryId('abcat0205000')}>Højtaler</button>
-<button onClick={() => categoryId('abcat0300000')}>Bil Electronik & GPS</button>
-<button onClick={() => categoryId('abcat0305000')}>Radar Detectors</button>
-<button onClick={() => categoryId('abcat0301005')}>"Marine GPS & Fishfinders</button>
+            <button onClick={() => categoryId('abcat0101000')}>Tv's</button>
+            <button onClick={() => categoryId('abcat0102000')}>Blu-ray & DVD Players</button>
+            <button onClick={() => categoryId('abcat0106021')}>Gamer Stole</button>
+            <button onClick={() => categoryId('abcat0200000')}>Lyd</button>
+            <button onClick={() => categoryId('abcat0201000')}>iPod & MP3 Players</button>
+            <button onClick={() => categoryId('abcat0204000')}>Headphones</button>
+            <button onClick={() => categoryId('abcat0205000')}>Højtaler</button>
+            <button onClick={() => categoryId('abcat0300000')}>Bil Electronik & GPS</button>
+            <button onClick={() => categoryId('abcat0305000')}>Radar Detectors</button>
+            <button onClick={() => categoryId('abcat0301005')}>"Marine GPS & Fishfinders</button>
             {
                 categoryData && categoryData.map((category, i) => {
                     return (
                         <div key={i}>
                             <div>
-                    <h1>{category.sku}</h1>
-                    <h1>{category.name}</h1>
-                    <h1>{category.type}</h1>
-                    <h1>{category.regularPrice}</h1>
-                    <h1>{category.salePrice}</h1>
-                    <a href={category.url}>Køb her</a>
-                    <a href={category.mobileUrl}>Køb her</a>
-                    <img src={category.image}></img>
-                    <h1>{category.onSale}</h1>
+                                <h1>{category.sku}</h1>
+                                <h1>{category.name}</h1>
+                                <h1>{category.type}</h1>
+                                <h1>{category.regularPrice}</h1>
+                                <h1>{category.salePrice}</h1>
+                                <a href={category.url}>Køb her</a>
+                                <img src={category.image}></img>
+                                <h1>{category.onSale}</h1>
 
-                            
+
                             </div>
                         </div>
                     );
@@ -52,20 +43,7 @@ const Category = () => {
         </div>
     );
 
-    };  
+};
 
 
 export default Category;
-
-
-
-
-
-
-
-
-
-//     var x, i = "";
-//for (i in data.products) {
-  //  setCategoryData("<h3>" + data.products[i].name + "</h3>")
-//}
--- src/product.js
+++ src/product.js
@@ -3,29 +3,28 @@ import React, { useState, useEffect } from "react";
 
 
 const Product = () => {
-  const [productData, setProductData] = useState("");
+    const [productData, setProductData] = useState("");
 
-  useEffect(() => {
-    facade.fetchProductData().then((data) => setProductData(data));
-  }, []);
+    useEffect(() => {
+        facade.fetchProductData().then((data) => setProductData(data));
+    }, []);
 
-  
-  return (
-    <div>
-      <h2>Product</h2>
-      <h3>{productData.sku}</h3>
-      <h3>{productData.name}</h3>
-      <h3>{productData.type}</h3>
-      <h3>{productData.regularPrice}</h3>
-      <h3>{productData.salePrice}</h3>
-      <a href={productData.url}>Køb her</a>
-      <a href={productData.mobileUrl}>Køb her</a>
-      <img src={productData.image}></img>
-      <h3>{productData.shortDesription}</h3>
-      <h3>{productData.onSale}</h3>
+    //<a href={productData.mobileUrl}>Køb her</a>  
+    return (
+        <div>
+            <h2>Product</h2>
+            <h3>{productData.sku}</h3>
+            <h3>{productData.name}</h3>
+            <h3>{productData.type}</h3>
+            <h3>{productData.regularPrice}</h3>
+            <h3>{productData.salePrice}</h3>
+            <a href={productData.url}>Køb her</a>
+            <img src={productData.image}></img>
+            <h3>{productData.shortDesription}</h3>
+            <h3>{productData.onSale}</h3>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 
